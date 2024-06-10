3af56a8d5d2d807f242731f5c0f07d2d4a99f4c1
Samet Candan <c._samet@hotmail.com>
2024-06-11 01:03:39 +0300

    list crud

---
+++ api/routes/lists.js
@@ -0,0 +1,28 @@
+const router = require("express").Router();
+
+const List = require("../models/List");
+
+const verify = require("../verifiyToken");
+
+//CREATE
+
+router.post("/", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    const newList = new List(req.body);
+
+    try {
+      const savedList = await newList.save();
+      res.status(201).json(savedList);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You are not allowed!");
+  }
+});
+
+//DELETE
+
+//GET
+
+module.exports = router;
